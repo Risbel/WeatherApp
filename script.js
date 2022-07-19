1a9ac96118b350b8b367e5e98b203e75cd7345d5
Risbel Suarez Rodriguez <risbel961019@gmail.com>
2022-07-19 05:22:20 +0200

    Puse los id

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 // vas a programar aqui, puedes usar los metodos de openweatherservice.js
-getWeatherData("Miami beach").then((data) => console.log(data));
+getWeatherData("Miami beach").then((data) => setWeatherData(data));
+
+const setWeatherData = data => {
+    console.log(data)
+    const weatherData = {
+        location: data.name,
+        temp: data.main.temp += "°",
+        humidity: data.main.humidity += "%",
+        date: getDate(),
+    }
+
+    Object.keys(weatherData).forEach ( key => {
+        document.getElementById(key).textContent = weatherData[key]
+    })
+  
+}
+
+ const getDate = () => {
+        let date = new Date()
+        return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`
+    }
