fe1110231183968a977a68ee9c303a0fc65fb65e
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2022-07-20 16:52:43 -0400

    feat: Add logic for appear/disappear spinner

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
 // vas a programar aqui, puedes usar los metodos de openweatherservice.js
 
-function load() {
-    const input = document.getElementById("search-input").value
-    return getWeatherData(input)
-        .then((data) => setWeatherData(data))        
+const toggleSpinnerOn = () => {
+  const spinner = document.getElementById("div-loading");
+  spinner.style.display = "flex";
 };
 
+const toggleSpinnerOff = () => {
+  const spinner = document.getElementById("div-loading");
+  spinner.style.display = "none";
+};
 
-const setWeatherData = data => {
-    console.log(data)
-    const ImgIcon = data.weather[0].icon
-            console.log(ImgIcon)
-            document.getElementById("weatherImg").src='http://openweathermap.org/img/wn/'+ ImgIcon +'@2x.png'
+function load() {
+  const input = document.getElementById("search-input").value;
+  return getWeatherData(input).then((data) => setWeatherData(data));
+}
 
-    const weatherData = {
-        location: data.name,
-        temp: data.main.temp += "°",
-        humidity: data.main.humidity += "%",
-        date: getDate(),
-        hour: time(),  
-    }
-    
-    Object.keys(weatherData).forEach( key => {
-        document.getElementById(key).textContent = weatherData[key]
-    })  
- 
+const setWeatherData = (data) => {
+  console.log(data);
+  const ImgIcon = data.weather[0].icon;
+  console.log(ImgIcon);
+  document.getElementById("weatherImg").src =
+    "http://openweathermap.org/img/wn/" + ImgIcon + "@2x.png";
+
+  const weatherData = {
+    location: data.name,
+    temp: (data.main.temp += "°"),
+    humidity: (data.main.humidity += "%"),
+    date: getDate(),
+    hour: time(),
+  };
+
+  Object.keys(weatherData).forEach((key) => {
+    document.getElementById(key).textContent = weatherData[key];
+  });
 };
 
 const getDate = () => {
-    let date = new Date()
-    return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
+  let date = new Date();
+  return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
 };
 const time = () => {
-    let date = new Date()
-    return `${date.getHours()}:${date.getMinutes()}`
+  let date = new Date();
+  return `${date.getHours()}:${date.getMinutes()}`;
 };
-
-
-
-
