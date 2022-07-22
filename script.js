f1f9ae0c89bb4c48c4bfe37a66d90cbf4c32c6df
Risbel Suarez Rodriguez <risbel961019@gmail.com>
2022-07-22 04:04:29 +0200

    spinner working

--- script.js
+++ script.js
@@ -1,45 +1,44 @@
 // vas a programar aqui, puedes usar los metodos de openweatherservice.js
 
-const toggleSpinnerOn = () => {
-  const spinner = document.getElementById("div-loading");
-  spinner.style.display = "flex";
-};
-
-const toggleSpinnerOff = () => {
-  const spinner = document.getElementById("div-loading");
-  spinner.style.display = "none";
-};
+ 
 
 function load() {
-  const input = document.getElementById("search-input").value;
-  return getWeatherData(input).then((data) => setWeatherData(data));
-}
+    const spinner = document.getElementById("div-loading");
+    spinner.style.display = "flex";
+    
+    const input = document.getElementById("search-input").value
+    return getWeatherData(input)
+        .then((data) => setWeatherData(data))        
+};
 
-const setWeatherData = (data) => {
-  console.log(data);
-  const ImgIcon = data.weather[0].icon;
-  console.log(ImgIcon);
-  document.getElementById("weatherImg").src =
-    "http://openweathermap.org/img/wn/" + ImgIcon + "@2x.png";
+const setWeatherData = data => {
+    console.log(data)
+    const ImgIcon = data.weather[0].icon
+            console.log(ImgIcon)
+            document.getElementById("weatherImg").src='http://openweathermap.org/img/wn/'+ ImgIcon +'@2x.png'
 
-  const weatherData = {
-    location: data.name,
-    temp: (data.main.temp += "°"),
-    humidity: (data.main.humidity += "%"),
-    date: getDate(),
-    hour: time(),
-  };
+    const weatherData = {
+        location: data.name,
+        temp: data.main.temp += "°",
+        humidity: data.main.humidity += "%",
+        date: getDate(),
+        hour: time(),  
+    }
+    
+    Object.keys(weatherData).forEach( key => {
+        document.getElementById(key).textContent = weatherData[key]
+    })  
 
-  Object.keys(weatherData).forEach((key) => {
-    document.getElementById(key).textContent = weatherData[key];
-  });
+
+    const spinner = document.getElementById("div-loading");
+    spinner.style.display = "none";
 };
 
 const getDate = () => {
-  let date = new Date();
-  return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+    let date = new Date()
+    return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
 };
 const time = () => {
-  let date = new Date();
-  return `${date.getHours()}:${date.getMinutes()}`;
+    let date = new Date()
+    return `${date.getHours()}:${date.getMinutes()}`
 };
