3e72a70c9e883dd272a731950d08d33ddebe66cc
Risbel Suarez <risbel961019@gmail.com>
2023-04-21 23:54:31 -0400

    feat: Add some styles & features

--- openweatherservice.js
+++ openweatherservice.js
@@ -3,10 +3,13 @@ const APP_ID = "ae1ba5d38aeff23990ba7232649a9865";
 
 // Estas funciones son accesibles desde script.js
 
+const spinner = document.getElementById("div-loading");
+
 const getWeatherData = (place) =>
-    fetch(`${BASE_URL}?q=${place}&APPID=${APP_ID}&units=metric`)
-        .then((response) => response.json())
-        .catch(err => alert(err.messaje))
-
-
-
+  fetch(`${BASE_URL}?q=${place}&APPID=${APP_ID}&units=metric`)
+    .then((response) => response.json())
+    .catch((err) => {
+      alert(err.messaje);
+      spinner.style.display = "none";
+      document.getElementById("error-message").innerHTML = `Uncaut error`;
+    });
--- script.js
+++ script.js
@@ -1,48 +1,65 @@
 // vas a programar aqui, puedes usar los metodos de openweatherservice.js
 function start(e) {
-    if (e.keyCode == 13) {
-        load()
-    }
+  if (e.keyCode == 13) {
+    load();
+  }
 }
 window.onkeypress = start;
 
 function load() {
-    const spinner = document.getElementById("div-loading");
+  const input = document.getElementById("search-input").value;
+  const spinner = document.getElementById("div-loading");
+  if (input != "") {
     spinner.style.display = "flex";
-    
-    const input = document.getElementById("search-input").value
-    return getWeatherData(input)
-        .then((data) => setWeatherData(data))        
-};
+    return getWeatherData(input).then((data) => setWeatherData(data));
+  } else {
+    return;
+  }
+}
 
-const setWeatherData = data => {
-    console.log(data)
-    const ImgIcon = data.weather[0].icon
-            console.log(ImgIcon)
-            document.getElementById("weatherImg").src='http://openweathermap.org/img/wn/'+ ImgIcon +'@2x.png'
+const setWeatherData = (data) => {
+  console.log(data);
+  const spinner = document.getElementById("div-loading");
+
+  if (data.cod == 404) {
+    spinner.style.display = "none";
+    document.getElementById("error-message").innerHTML = `${data.message}`;
+  } else {
+    document.getElementById("error-message").innerHTML = "";
+
+    const ImgIcon = data.weather[0].icon;
+    console.log(ImgIcon);
+    document.getElementById("weatherImg").src = "http://openweathermap.org/img/wn/" + ImgIcon + "@2x.png";
 
     const weatherData = {
-        location: data.name,
-        temp: data.main.temp += "°",
-        humidity: data.main.humidity += "%",
-        date: getDate(),
-        hour: time(),  
-    }
-    
-    Object.keys(weatherData).forEach( key => {
-        document.getElementById(key).textContent = weatherData[key]
-    })  
+      location: data.name,
+      temp: (data.main.temp += "°"),
+      humidity: (data.main.humidity += "%"),
+      date: getDate(),
+      hour: time(data.timezone),
+    };
 
+    Object.keys(weatherData).forEach((key) => {
+      document.getElementById(key).textContent = weatherData[key];
+    });
+  }
 
-    const spinner = document.getElementById("div-loading");
-    spinner.style.display = "none";
+  spinner.style.display = "none";
 };
 
 const getDate = () => {
-    let date = new Date()
-    return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
+  let date = new Date();
+  return `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
 };
-const time = () => {
-    let date = new Date()
-    return `${date.getHours()} : ${("0" + date.getMinutes()).slice(-2)}`
+
+const hours = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const time = (timezone) => {
+  let date = new Date();
+  let hoursUTC = date.getUTCHours();
+  let minutesUTC = date.getUTCMinutes();
+  let timezoneToHour = timezone / 3600;
+  let selector = hoursUTC + timezoneToHour;
+  let realHour = `${hoursUTC} + (${timezoneToHour})`;
+  return `${realHour} : ${minutesUTC}`;
 };
